d722740d54186eccef7c9af9626d9f257e232232
Sai Tejaswini <S533726@nwmissouri.edu>
2019-03-11 16:12:56 -0500

    commit changes of some activities

---
+++ GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/DealsoftheDayActivity.java
@@ -0,0 +1,13 @@
+package com.example.getveggiesfruits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DealsoftheDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dealsofthe_day);
+    }
+}
