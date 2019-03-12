5eac583c35760c6ab2681cc6a6dd3b3906a1d20e
Sai Tejaswini <S533726@nwmissouri.edu>
2019-03-12 12:20:38 -0500

    commit changes for layouts

--- GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/OrderSummary.java
+++ GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/OrderSummary.java
@@ -12,7 +12,22 @@ public class OrderSummary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordersummary);
         TextView btn = (TextView) findViewById(R.id.name);
-        btn.setText("Saitejaswini Narne");
+        btn.setText("Sai Tejaswini Narne");
+
+        TextView btn1 = (TextView) findViewById(R.id.billingid);
+        btn1.setText("10354");
+
+        TextView btn2 = (TextView) findViewById(R.id.tracking);
+        btn2.setText("12324354655");
+
+        TextView btn3 = (TextView) findViewById(R.id.Address);
+        btn3.setText("1115 N College Dr #100 " +
+                "Maryville " +
+                "MO " +
+                "64468  ");
+
+
+
 
     }
 
