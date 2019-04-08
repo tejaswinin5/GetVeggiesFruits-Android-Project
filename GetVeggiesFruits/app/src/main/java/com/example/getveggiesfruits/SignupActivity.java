ccc1bdfa3945e68866bcb3307ea333eb00f13e9c
Akshara Gurram <s533729@nwmissouri.edu>
2019-04-08 13:09:33 -0500

    added signup logic in the SignupActivity.java

--- GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/SignupActivity.java
+++ GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/SignupActivity.java
@@ -27,3 +27,17 @@ public class SignupActivity extends AppCompatActivity {
         edPassword = findViewById(R.id.edPassword);
         edConfirmPassword = findViewById(R.id.edConfirmPassword);
     }
+
+    public void signup(View view) {
+        if( TextUtils.isEmpty(edName.getText())){
+            edName.setError( "Name is required!" );
+        }else if( TextUtils.isEmpty(edEmail.getText())){
+            edEmail.setError( "Email is required!" );
+        }else if( TextUtils.isEmpty(edPassword.getText())){
+            edPassword.setError( "Password is required!" );
+        }else if( TextUtils.isEmpty(edConfirmPassword.getText())){
+            edConfirmPassword.setError( "Confirm password is required!" );
+        }else if(!edPassword.getText().toString().equals(edConfirmPassword.getText().toString())){
+            Toast.makeText(SignupActivity.this, "Passwords are not the same!", Toast.LENGTH_LONG).show();
+        }
+        
