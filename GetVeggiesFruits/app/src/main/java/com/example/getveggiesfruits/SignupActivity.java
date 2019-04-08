54c64123dd881072dacca3b5046675d5ada15dd3
Harika Naidu <nagaharika.n@gmail.com>
2019-04-08 12:23:46 -0500

    updated the SignupActivity.java with the intent

---
+++ GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.example.getveggiesfruits;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
