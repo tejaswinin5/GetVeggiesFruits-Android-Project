b2c164a43dd0b124f8cf76fb7a732455aa7b684b
Akshara Gurram <s533729@nwmissouri.edu>
2019-04-28 19:48:28 -0500

    Added spinners in Billing activity for the menu display options.

--- GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/BillingActivity.java
+++ GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/BillingActivity.java
@@ -33,3 +33,76 @@ public class BillingActivity extends AppCompatActivity {
         mspinner.setSelection(0);
         ArrayAdapter<String> myAdpater2=new ArrayAdapter<String>(BillingActivity.this,android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.names1));
+        myAdpater.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mspinner2.setAdapter(myAdpater2);
+
+        Button btn = (Button) findViewById (R.id.continueBUT);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView customerFName=(TextView)findViewById(R.id.editText2);
+                TextView customerLName=(TextView)findViewById(R.id.editText3);
+                TextView customerAddress=(TextView)findViewById(R.id.editText4);
+                TextView customerNumber=(TextView)findViewById(R.id.cardNumber);
+                TextView cardName=(TextView)findViewById(R.id.nameOfCard);
+                TextView expiry=(TextView)findViewById(R.id.expiry);
+                TextView cvv=(TextView)findViewById(R.id.securityText);
+                long cardLong=0;
+                if(!(customerNumber.getText().toString().equals(""))) {
+                    cardLong = Long.parseLong(customerNumber.getText().toString());
+                }
+                if(customerFName.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "First Name is empty", Toast.LENGTH_SHORT).show();
+                }
+                else if(customerAddress.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Enter the address field to proceed further", Toast.LENGTH_SHORT).show();
+                }
+                else if (!isValid(cardLong)) {
+                    Toast.makeText(getApplicationContext(), "Card is invalid", Toast.LENGTH_SHORT).show();
+                }
+                else if(cardName.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Enter the Name of the card", Toast.LENGTH_SHORT).show();
+                }
+                else if(expiry.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Expiry is invalid", Toast.LENGTH_SHORT).show();
+                }
+                else if(cvv.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "CVV is invalid", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent ini = new Intent(BillingActivity.this, OrderSummary.class);
+                    ini.putExtra("CustomerFName", customerFName.getText().toString());
+                    ini.putExtra("CustomerLName", customerLName.getText().toString());
+                    ini.putExtra("CustomerAddress", customerAddress.getText().toString());
+                    ParseObject billing = new ParseObject("BillingDetails");
+                    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                    Date date = new Date();
+                    System.out.println(dateFormat.format(date));
+                    billing.put("CustomerFName",customerNumber.getText().toString());
+                    billing.put("CustomerLName",customerLName.getText().toString());
+                    billing.put("CustomerAddress",customerAddress.getText().toString());
+                    billing.put("OrderDate",dateFormat.format(date));
+                    billing.put("PurchaseAmount","8");
+                    billing.put("PurchaseItems","Tomatoes Apples");
+                    startActivityForResult(ini, 1);
+                    billing.saveInBackground(new SaveCallback()
+                    {
+                        @Override public void done(ParseException e)
+                        {
+                            System.out.println("Saved Callback");
+                        }
+                    });
+                }
+            }
+        });
+
+        Button btn1 = (Button) findViewById(R.id.backBUT);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("new intent can be created");
+                Intent intent = getIntent();
+                finish();
+            }
+        });
+    }
