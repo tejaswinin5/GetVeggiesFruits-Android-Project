decd61fd690ab4e08996b3dca7d98e7b85eaf1a2
Akshara Gurram <s533729@nwmissouri.edu>
2019-04-28 19:51:05 -0500

    Modified the credit card validation logic

--- GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/BillingActivity.java
+++ GetVeggiesFruits/app/src/main/java/com/example/getveggiesfruits/BillingActivity.java
@@ -123,3 +123,12 @@ public class BillingActivity extends AppCompatActivity {
                 ((sumOfDoubleEvenPlace(number) +
                         sumOfOddPlace(number)) % 10 == 0);
     }
+    public static int sumOfDoubleEvenPlace(long number)
+    {
+        int sum = 0;
+        String num = number + "";
+        for (int i = getSize(number) - 2; i >= 0; i -= 2)
+            sum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2);
+
+        return sum;
+    }
